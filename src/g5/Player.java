cce77a93fd7cbde07e728bc06e0a9ac55137f6c6
Sharon <sharonjin17@gmail.com>
2020-09-16 10:19:35 -0400

    wrote naive function to reallocate scores

--- src/g5/Player.java
+++ src/g5/Player.java
@@ -1,7 +1,9 @@
 package g5; // TODO modify the package name to reflect your team
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 
 import sim.Game;
 import sim.GameHistory;
@@ -33,8 +35,63 @@ public class Player extends sim.Player {
      *
      */
     public List<Game> reallocate(Integer round, GameHistory gameHistory, List<Game> playerGames, Map<Integer, List<Game>> opponentGamesMap) {
-        // TODO add your code here to reallocate player goals
+        int excessGoals = 0;
+        List<Game> reallocatedGames = new ArrayList<Game>();
+        List<Game> losingGames = new ArrayList<Game>();
+        List<Game> tieGames = new ArrayList<Game>();
+        List<Game> winningGames = new ArrayList<Game>();
 
-        return null; // TODO modify the return statement to return your list of reallocated player games
+        for (Game game : playerGames) {
+            int playerGoals = game.getNumPlayerGoals();
+            int margin = playerGoals - game.getNumOpponentGoals();
+            if (margin > 0) {
+                int subtract = Math.min(margin-1, game.getHalfNumPlayerGoals());
+                excessGoals += subtract;
+                game.setNumPlayerGoals(playerGoals-subtract);
+                winningGames.add(game);
+            }
+            else if (margin == 0) {
+                if (excessGoals > 0 && playerGoals < 8) {
+                    excessGoals -= 1;
+                    game.setNumPlayerGoals(playerGoals+1);
+                }
+                tieGames.add(game);
+            }
+            else {
+                losingGames.add(game);
+            }
+        }
+
+        losingGames.sort(Comparator.comparingInt(g -> (g.getNumOpponentGoals() - g.getNumPlayerGoals())));
+
+        for (Game game : losingGames) {
+            int opponentGoals = game.getNumOpponentGoals();
+            int margin = opponentGoals - game.getNumPlayerGoals();
+            if (excessGoals > margin && opponentGoals < 8) {
+                excessGoals -= margin+1;
+                game.setNumPlayerGoals(margin+1);
+            }
+            reallocatedGames.add(game);
+        }
+
+        for (Game game : tieGames) {
+            int playerGoals = game.getNumPlayerGoals();
+            if (excessGoals > 0 && playerGoals < 8) {
+                excessGoals -= 1;
+                game.setNumPlayerGoals(playerGoals+1);
+            }
+            reallocatedGames.add(game);
+        }
+
+        for (Game game : winningGames) {
+            int playerGoals = game.getNumPlayerGoals();
+            if (excessGoals > 0 && playerGoals < 8) {
+                excessGoals -= 1;
+                game.setNumPlayerGoals(playerGoals+1);
+            }
+            reallocatedGames.add(game);
+        }
+
+        return reallocatedGames;
     }
 }
