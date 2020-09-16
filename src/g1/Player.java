6882bd5e0d0084a92bcddd627124572001812b6d
Spencer Bruce <spencer.bruce@columbia.edu>
2020-09-16 00:15:57 -0400

    First deliverable, Player allocates max number of goals to random game

--- src/g1/Player.java
+++ src/g1/Player.java
@@ -1,5 +1,6 @@
 package g1; // TODO modify the package name to reflect your team
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +35,127 @@ public class Player extends sim.Player {
       */
      public List<Game> reallocate(Integer round, GameHistory gameHistory, List<Game> playerGames, Map<Integer, List<Game>> opponentGamesMap) {
           // TODO add your code here to reallocate player goals
+
+          //don't use:
+          //nothing with gameHistory
+          //disregard opponentGamesMap
+               // used to detect trends in opponent's reallocations 
           
-          return null; // TODO modify the return statement to return your list of reallocated player games
+          
+          //use:
+          //player games, indexed by round
+          //how many we have available to reallocate?
+          
+          
+          // if our team won, remove as many points as possible (half at most)
+          // this ensures a win while maximizing points 
+
+
+          // if it was a draw, either remove all points or do not remove any and hope the opponent removes points
+          
+          // return null; // TODO modify the return statement to return your list of reallocated player games
+
+             // strategy for future deliverable? vvv
+          //sort by won games how much we won by
+		//sort lost games by how much we lost by
+		//match these up
+
+          // if we won:
+               // take away our score - opponents score + 1 so long as our score / 2 is not > this number 
+           
+          List<Game> reallocatedPlayerGames = new ArrayList<>();
+          
+          List<Game> wonGames = getWinningGames(playerGames);
+          List<Game> drawnGames = getDrawnGames(playerGames);
+          List<Game> lostGames = getLosingGames(playerGames);
+          
+          List<Game> lostOrDrawnGamesWithReallocationCapacity = new ArrayList<>(lostGames);
+          lostOrDrawnGamesWithReallocationCapacity.addAll(drawnGames);
+          for(Game lostGame : lostGames)
+               if(lostGame.maxPlayerGoalsReached())
+                    lostOrDrawnGamesWithReallocationCapacity.remove(lostGame);
+          for(Game drawnGame : drawnGames)
+               if(drawnGame.maxPlayerGoalsReached())
+                    lostOrDrawnGamesWithReallocationCapacity.remove(drawnGame);
+          
+               
+          
+
+               //if we're more than double the score of the opponent, remove score/2
+               //else remove our score-their score - 1
+
+               //allocate to random game
+
+          for(Game winningGame : wonGames) {    		 
+               
+               if(lostOrDrawnGamesWithReallocationCapacity.size() == 0)
+                    break;
+
+               Game randomLostOrDrawnGame = lostOrDrawnGamesWithReallocationCapacity.get(this.random.nextInt(lostOrDrawnGamesWithReallocationCapacity.size()));
+
+               int halfNumPlayerGoals = winningGame.getHalfNumPlayerGoals();
+               //int numRandomGoals = (int) Math.min(this.random.nextInt(halfNumPlayerGoals) + 1, Game.getMaxGoalThreshold() - randomLostOrDrawnGame.getNumPlayerGoals());
+
+			   int numRandomGoals;
+
+			   int ourScore = winningGame.getScore().getNumPlayerGoals();
+			   int theirScore = winningGame.getScore().getNumOpponentGoals();
+
+			   if(halfNumPlayerGoals > theirScore) {
+				   numRandomGoals = halfNumPlayerGoals;
+			   }
+			   else {
+				   numRandomGoals = ourScore - theirScore - 1;
+			   }
+
+               winningGame.setNumPlayerGoals(winningGame.getNumPlayerGoals() - numRandomGoals);
+               randomLostOrDrawnGame.setNumPlayerGoals(randomLostOrDrawnGame.getNumPlayerGoals() + numRandomGoals);
+               
+               if(randomLostOrDrawnGame.maxPlayerGoalsReached())
+                    lostOrDrawnGamesWithReallocationCapacity.remove(randomLostOrDrawnGame);
+          }
+          
+          reallocatedPlayerGames.addAll(wonGames);
+          reallocatedPlayerGames.addAll(drawnGames);
+          reallocatedPlayerGames.addAll(lostGames);
+
+          if(checkConstraintsSatisfied(playerGames, reallocatedPlayerGames))
+               return reallocatedPlayerGames;
+          return playerGames;
+
+     }
+
+     // stolen from random >:D
+     private List<Game> getWinningGames(List<Game> playerGames) {
+    	 List<Game> winningGames = new ArrayList<>();
+    	 for(Game game : playerGames) {
+    		 int numPlayerGoals = game.getNumPlayerGoals();
+    		 int numOpponentGoals = game.getNumOpponentGoals();
+    		 if(numPlayerGoals > numOpponentGoals)
+    			 winningGames.add(game.cloneGame());
+    	 }
+    	 return winningGames;
+     }
+
+     private List<Game> getDrawnGames(List<Game> playerGames) {
+    	 List<Game> drawnGames = new ArrayList<>();
+    	 for(Game game : playerGames) {
+    		 int numPlayerGoals = game.getNumPlayerGoals();
+    		 int numOpponentGoals = game.getNumOpponentGoals();
+    		 if(numPlayerGoals == numOpponentGoals)
+    			 drawnGames.add(game.cloneGame());
+    	 }
+    	 return drawnGames;
+     }
+     
+     private List<Game> getLosingGames(List<Game> playerGames) {
+    	 List<Game> losingGames = new ArrayList<>();
+    	 for(Game game : playerGames) {
+    		 int numPlayerGoals = game.getNumPlayerGoals();
+    		 int numOpponentGoals = game.getNumOpponentGoals();
+    		 if(numPlayerGoals < numOpponentGoals)
+    			 losingGames.add(game.cloneGame());
+    	 }
+    	 return losingGames;
      }
 }
