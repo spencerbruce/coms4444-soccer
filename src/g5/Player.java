89e57c12d072bfa4b552847cb5aafe389bba1f65
Sharon <sharonjin17@gmail.com>
2020-09-15 21:16:50 -0400

    added player file for group 5

---
+++ src/g5/Player.java
@@ -0,0 +1,40 @@
+package g5; // TODO modify the package name to reflect your team
+
+import java.util.List;
+import java.util.Map;
+
+import sim.Game;
+import sim.GameHistory;
+import sim.SimPrinter;
+
+public class Player extends sim.Player {
+
+    /**
+     * Player constructor
+     *
+     * @param teamID      team ID
+     * @param rounds      number of rounds
+     * @param seed        random seed
+     * @param simPrinter  simulation printer
+     *
+     */
+    public Player(Integer teamID, Integer rounds, Integer seed, SimPrinter simPrinter) {
+        super(teamID, rounds, seed, simPrinter);
+    }
+
+    /**
+     * Reallocate player goals
+     *
+     * @param round             current round
+     * @param gameHistory       cumulative game history from all previous rounds
+     * @param playerGames       state of player games before reallocation
+     * @param opponentGamesMap  state of opponent games before reallocation (map of opponent team IDs to their games)
+     * @return                  state of player games after reallocation
+     *
+     */
+    public List<Game> reallocate(Integer round, GameHistory gameHistory, List<Game> playerGames, Map<Integer, List<Game>> opponentGamesMap) {
+        // TODO add your code here to reallocate player goals
+
+        return null; // TODO modify the return statement to return your list of reallocated player games
+    }
+}
