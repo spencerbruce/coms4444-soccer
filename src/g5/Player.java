c810362f88a89ac4fd0b4ac1682dfa0056973657
Sharon <sharonjin17@gmail.com>
2020-09-20 23:33:22 -0400

    Added functionality to take ranking into account

--- src/g5/Player.java
+++ src/g5/Player.java
@@ -24,6 +24,20 @@ public class Player extends sim.Player {
         super(teamID, rounds, seed, simPrinter);
     }
 
+    // get game IDs where opponents have higher ranks
+    public List<Integer> getGamesToBeatIDs(Integer round, GameHistory gameHistory) {
+        List<Integer> gamesToBeat = new ArrayList<Integer>();
+        Map<Integer, Double> rankings = gameHistory.getAllAverageRankingsMap().get(round-1);
+
+        for (Map.Entry<Integer, Double> ranking : rankings.entrySet()) {
+            Integer otherTeamID = ranking.getKey();
+            if (!otherTeamID.equals(teamID) && ranking.getValue() <= rankings.get(teamID)) {
+                gamesToBeat.add(otherTeamID);
+            }
+        }
+        return gamesToBeat;
+    }
+
     /**
      * Reallocate player goals
      *
@@ -35,6 +49,12 @@ public class Player extends sim.Player {
      *
      */
     public List<Game> reallocate(Integer round, GameHistory gameHistory, List<Game> playerGames, Map<Integer, List<Game>> opponentGamesMap) {
+        List<Integer> gamesToBeat = new ArrayList<Integer>();
+        if (round > 1) {
+            // get game IDs where opponents have higher ranks
+            gamesToBeat = getGamesToBeatIDs(round, gameHistory);
+        }
+
         int excessGoals = 0;
         List<Game> reallocatedGames = new ArrayList<Game>();
         List<Game> losingGames = new ArrayList<Game>();
@@ -53,6 +73,12 @@ public class Player extends sim.Player {
             }
             // get tied games and allocate 1 if max score hasn't yet been reached
             else if (margin == 0) {
+                // if against opponent that is ranked higher, allocate more points
+                if (excessGoals > 0 && playerGoals < 8 && gamesToBeat.contains(game.getID())) {
+                    excessGoals -= 1;
+                    game.setNumPlayerGoals(playerGoals+1);
+                    playerGoals += 1;
+                }
                 if (excessGoals > 0 && playerGoals < 8) {
                     excessGoals -= 1;
                     game.setNumPlayerGoals(playerGoals+1);
@@ -71,10 +97,16 @@ public class Player extends sim.Player {
         // reallocate goals to the losing games
         for (Game game : losingGames) {
             int opponentGoals = game.getNumOpponentGoals();
+            int playerGoals = game.getNumPlayerGoals();
             int margin = opponentGoals - game.getNumPlayerGoals();
             if (excessGoals > margin && opponentGoals < 8) {
                 excessGoals -= margin+1;
                 game.setNumPlayerGoals(margin+1);
+            }
+            // if against opponent that is ranked higher, allocate more points
+            if (excessGoals > 0 && playerGoals < 8 && gamesToBeat.contains(game.getID())) {
+                excessGoals -= 1;
+                game.setNumPlayerGoals(playerGoals+1);
             }
             reallocatedGames.add(game);
         }
